a294fd99b5ae5a50443121baa9ac4c13f0293840
myevan <myevan@outlook.com>
2019-08-25 14:20:05 +0900

    core: components renumbered

--- core/numbers.mjs
+++ core/numbers.mjs
@@ -7,8 +7,8 @@ export const NK_Down = 2;
 export const NK_Left = 4;
 export const NK_Right = 8;
 
-export const NC_Identity = 1;
-export const NC_Transform = 2;
-export const NC_TextScreen = 3;
-export const NC_Landscape = 4;
-export const NC_Stage = 5;
+export const NC_Identity = 11;
+export const NC_Transform = 12;
+export const NC_TextScreen = 13;
+export const NC_Landscape = 14;
+export const NC_Stage = 15;
